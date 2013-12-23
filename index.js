3cbde5aa4da7ddce1ab71665c1a96d60d93123a4
Cheng Lou <chenglou92@gmail.com>
2013-12-23 15:16:57 -0500

    small typo correction

--- index.js
+++ index.js
@@ -2,4 +2,4 @@
 var React = require('react');
 var pkg = require('./package.json');
 
-React.renderComponent(<h1>{pkg.name}, brought to by React!</h1>, document.body);
+React.renderComponent(<h1>{pkg.name}, brought to you by React!</h1>, document.body);
